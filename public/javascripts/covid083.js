c1936e9bc0c2e5de0d6bc7cbabd7c1835fc48828
naemsupawat083 <71489780+naemsupawat083@users.noreply.github.com>
2021-11-15 21:23:04 +0700

    put wai kon

--- public/javascripts/covid083.js
+++ public/javascripts/covid083.js
@@ -6,7 +6,6 @@
         covid19data = data;
     }
     getapi();
-
     function onClickFunction(){
         console.log("data");
         let birthday =  document.getElementById("dateInput").value;
@@ -17,7 +16,6 @@
         let searchcovid19data = covid19data.find((data) => {
             if (data.txn_date === birthday && data.province === provinceTH) return true;
         })
-
             document.getElementById('txn_date').innerHTML = searchcovid19data.txn_date;
             document.getElementById('province').innerHTML = searchcovid19data.province;
             document.getElementById('new_case').innerHTML = searchcovid19data.new_case;
@@ -28,21 +26,27 @@
             document.getElementById('total_death').innerHTML = searchcovid19data.total_death;
             console.log(searchcovid19data);
     }
-    function onClickFunction2(){
-        let birthday =  document.getElementById("dateInput").value;
+    
+    function onClickFunction2(data){
+        let birthday =  document.getElementById("dateInput2").value;
         let searchcovid19data = covid19data.find((data) => {
         if (data.txn_date === birthday) return true;
     })
-        console.log(searchcovid19data);
-
+            
+            console.log(searchcovid19data);
+    
+            document.getElementById('tableTxn').innerHTML += `
+                    <tr>
+                        <th scope="row" id="myRowTable">${i}</th>
+                        <td colspan="2" id="province2">${searchcovid19data.province[i]}</td>
+                        <td  id="new_case2">-</td>
+                        <td id="total_case2">-</td>
+                        <td id="new_ death2">-</td>
+                        <td id="total_death2">-</td>
+                        <td id="new_case_excludeabroad2">-</td>
+                        <td id="total_case_excludeabroad2">-</td>
+                    </tr>
+            `
+        }
         
-            document.getElementById('txn_date2').innerHTML = searchcovid19data.txn_date;
-            document.getElementById('province2').innerHTML = searchcovid19data.province;
-            document.getElementById('new_case2').innerHTML = searchcovid19data.new_case;
-            document.getElementById('total_case2').innerHTML = searchcovid19data.total_case;
-            document.getElementById('new_case_excludeabroad2').innerHTML = searchcovid19data.new_case_excludeabroad;
-            document.getElementById('total_case_excludeabroad2').innerHTML = searchcovid19data.total_case_excludeabroad;
-            document.getElementById('new_ death2').innerHTML = searchcovid19data.new_death;
-            document.getElementById('total_death2').innerHTML = searchcovid19data.total_death;
-    }
     
