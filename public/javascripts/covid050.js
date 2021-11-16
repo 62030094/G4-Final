7845c753befd9eb10877a55f5851dc507f4a72eb
62030099kmitl <62030099kmitl@kmitl.ac.th>
2021-11-16 11:33:03 +0700

    Change 050 to 099

--- public/javascripts/covid050.js
+++ public/javascripts/covid050.js
@@ -1,81 +1,27 @@
-function dateSearch(){
-    document.getElementById("tabledata").innerHTML=`<tr class="head">
-    <th scope="col"><b>&nbsp;ลำดับ&nbsp;</b></th>
-    <th scope="col" colspan="2">&nbsp;เพศ&nbsp;</th>
-    <th scope="col">&nbsp;อายุ&nbsp;</th>
-    <th scope="col">&nbsp;ช่วงอายุ&nbsp;</th>
-    <th scope="col">&nbsp;เชื้อชาติ&nbsp;</th>
-    <th scope="col">&nbsp;อาชีพ&nbsp;</th>
-    <th scope="col">&nbsp;สาเหตุความเสี่ยง&nbsp;</th>
-    <th scope="col">&nbsp;ประเภทผู้ป่วย&nbsp;</th>
-    <th scope="col">&nbsp;จังหวัด&nbsp;</th>
-    <th scope="col">&nbsp;วันที่อัพเดท&nbsp;</th>
-  </tr>`;
+let covid19data = [];
 
-    var search = document.getElementById("inputdate").value;
-    console.log(search);
-    const getdata = ("https://covid19.ddc.moph.go.th/api/Cases/round-3-line-lists");
-    showdata();
-
-async function showdata(){
-    const response = await fetch(getdata);
+async function getapi() {
+    const response = await fetch("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
     const data = await response.json();
-
-
-    var dateId  = data.data;
-    console.log(dateId)
-
-    var itemData = dateId.filter(itemData => itemData.txn_date == search);
-
-    console.log(itemData)
-
-    for (var i=0;i<itemData.length-1;i++)
-    {
-        var sexTd = itemData[i].gender;
-        var ageTd = itemData[i].age_number;
-        var ageRangeTd = itemData[i].age_range;
-        var nationTd = itemData[i].nationality;
-        var jobTd = itemData[i].job;
-        var riskTd = itemData[i].risk;
-        var patientTd = itemData[i].patient_type;
-        var provinceTd = itemData[i].province;
-        var updateTxnTd = itemData[i].update_date;
-
-        document.getElementById("tabledata").innerHTML += `<tr>
-        <th scope="row">${i+1}</th>
-        <td colspan="2" id="province2">${sexTd}</td>
-        <td>${ageTd}</td>
-        <td>${ageRangeTd}</td>
-        <td>${nationTd}</td>
-        <td>${jobTd}</td>
-        <td>${riskTd}</td>
-        <td>${patientTd}</td>
-        <td>${provinceTd}</td>
-        <td>${updateTxnTd}</td>
-      </tr>`
-    }
-    
-        // var sextb=column[2];
-        // var agetb=column[3];
-        // var provincetb=column[9];
-        // var nationtb=column[5];
-        // var risktb=column[7];
-        // var pattb=column[8];
-
-      //   document.getElementById("tabledata").innerHTML+= `<tr>
-      //   <th scope="row">${i+1}</th>
-      //   <td colspan="2" id="province2">&nbsp;Mark&nbsp;</td>
-      //   <td>&nbsp;Otto&nbsp;</td>
-      //   <td>&nbsp;@mdo&nbsp;</td>
-      //   <td>&nbsp;Mark&nbsp;</td>
-      //   <td>&nbsp;Otto&nbsp;</td>
-      //   <td>&nbsp;@mdo&nbsp;</td>
-      //   <td>&nbsp;@mdo&nbsp;</td>
-      //   <td>&nbsp;@mdo&nbsp;</td>
-      //   <td>&nbsp;@mdo&nbsp;</td>
-      // </tr>`
-    }
-
-
+    covid19data = data;
+    console.log(data);
 }
 
+getapi();
+
+function showData() {
+    let provinceTH = document.getElementById("province").value;
+    console.log(provinceTH)
+
+    let searchcovid19data = covid19data.find((data) => {
+        if (data.province === provinceTH) return true;
+    })
+
+    console.log(searchcovid19data)
+    document.getElementById("newCase").innerHTML = searchcovid19data.new_case;
+    document.getElementById("totalCase").innerHTML = searchcovid19data.total_case;
+    document.getElementById("newCaseEx").innerHTML = searchcovid19data.new_case_excludeabroad;
+    document.getElementById("totalCaseEx").innerHTML = searchcovid19data.total_case_excludeabroad;
+    document.getElementById("newDeath").innerHTML = searchcovid19data.new_death;
+    document.getElementById("totalDeath").innerHTML = searchcovid19data.total_death;
+}
--- public/javascripts/covid099.js
+++ public/javascripts/covid099.js
@@ -1,27 +1,61 @@
-let covid19data = [];
+function dateSearch(){
+    document.getElementById("tabledata").innerHTML=`<tr class="head">
+    <th>ลำดับ</th>
+    <th>เพศ</th>
+    <th>อายุ</th>
+    <th>เชื้อชาติ</th>
+    <th>อาชีพ</th>
+    <th>สาเหตุความเสี่ยง</th>
+    <th>ประเภทผู้ป่วย</th>
+    <th>จังหวัด</th>
+  </tr>`;
 
-async function getapi() {
-    const response = await fetch("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
+    var search = document.getElementById("inputdate").value;
+    var provinceTH = document.getElementById("provinceTH").value;
+    console.log(search);
+    const getdata = ("https://covid19.ddc.moph.go.th/api/Cases/round-3-line-lists");
+    showdata();
+
+async function showdata(){
+    const response = await fetch(getdata);
     const data = await response.json();
-    covid19data = data;
-    console.log(data);
+
+    var dateId  = data.data;
+    console.log(dateId)
+
+    if (provinceTH != "")
+    {
+      var itemData = dateId.filter(itemData => itemData.txn_date == search && itemData.province == provinceTH);
+    }else var itemData = dateId.filter(itemData => itemData.txn_date == search);
+    
+
+    console.log(itemData)
+
+    if (itemData.length!=0)
+    {
+      for (var i=0;i<itemData.length;i++)
+      {
+        var sexTd = itemData[i].gender;
+        var ageTd = itemData[i].age_number;
+        var nationTd = itemData[i].nationality;
+        var jobTd = itemData[i].job;
+        var riskTd = itemData[i].risk;
+        var patientTd = itemData[i].patient_type;
+        var provinceTd = itemData[i].province;
+
+        document.getElementById("tabledata").innerHTML += `<tr>
+        <th scope="row">${i+1}</th>
+          <td>${sexTd}</td>
+          <td>${ageTd}</td>
+          <td>${nationTd}</td>
+          <td>${jobTd}</td>
+          <td>${riskTd}</td>
+          <td>${patientTd}</td>
+          <td>${provinceTd}</td>
+      </tr>`
+        document.getElementById("dontFind").style.visibility = "hidden";
+      }
+    }else {document.getElementById("dontFind").style.visibility = "visible";}
+  }
 }
 
-getapi();
-
-function showData() {
-    let provinceTH = document.getElementById("province").value;
-    console.log(provinceTH)
-
-    let searchcovid19data = covid19data.find((data) => {
-        if (data.province === provinceTH) return true;
-    })
-
-    console.log(searchcovid19data)
-    document.getElementById("newCase").innerHTML = searchcovid19data.new_case;
-    document.getElementById("totalCase").innerHTML = searchcovid19data.total_case;
-    document.getElementById("newCaseEx").innerHTML = searchcovid19data.new_case_excludeabroad;
-    document.getElementById("totalCaseEx").innerHTML = searchcovid19data.total_case_excludeabroad;
-    document.getElementById("newDeath").innerHTML = searchcovid19data.new_death;
-    document.getElementById("totalDeath").innerHTML = searchcovid19data.total_death;
-}
